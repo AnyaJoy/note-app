c51668f7c8f64fa71388284e6ccb345b6c16e16c
AnyaJoy <88624249+AnyaJoy@users.noreply.github.com>
2021-11-23 14:12:14 +0200

    unarchive button and function

--- src/Components/ArchiveNoteList.js
+++ src/Components/ArchiveNoteList.js
@@ -3,18 +3,47 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 export default function NoteList(props) {
- //recieves props
- const { noteData, setNoteData, disabled, setDisabled, archiveNoteData, setArchiveNoteData } = props;
+  //recieves props
+  const {
+    noteData,
+    setNoteData,
+    disabled,
+    setDisabled,
+    archiveNoteData,
+    setArchiveNoteData,
+  } = props;
+
+  const handleReturn = (e) => {
+    const result = window.confirm(
+      "Are you sure you want to unarchivate your note?"
+    );
+    if (result) {
+      var obj = archiveNoteData.find((obj) => obj.id === e.target.value);
+      setNoteData((prevNotes) => {
+        return [obj, ...prevNotes];
+      });
+
+      let newArchiveNoteData = archiveNoteData.filter((item) => {
+        return item.id !== e.target.value;
+      });
+      setArchiveNoteData(newArchiveNoteData);
+    }
+  };
+
   return (
     <>
       {archiveNoteData.map((item) => {
         return (
           <div key={item.id} className="note-wrapper">
-            {/* <img
-              className={`archive-img-${!disabled}`}
-              onClick={handleArchive}
-              src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-archive-office-stationery-justicon-flat-justicon.png"
-            /> */}
+            <button
+              className="archive-button-true"
+              onClick={handleReturn}
+              value={item.id}
+            ></button>
+            <img
+              className="archive-img"
+              src="https://img.icons8.com/dusk/64/ffffff/return.png"
+            />
             {item.title && <div className="title">{item.title}</div>}
             <div className="note-title">{item.note}</div>
             <div className="date-delete-wrapper">
